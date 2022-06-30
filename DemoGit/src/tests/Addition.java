ee2e64e766cb139f75f21cbef57388cfcf96be90
dimpalgit <dimpal.r.yadav.99@gmail.com>
2022-06-30 19:59:40 +0530

    first commit

---
+++ DemoGit/src/tests/Addition.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20,c;
+		c=a+b;
+		System.out.println("+c");
+
+	}
+
+}
---
+++ DemoGit/src/tests/Calculator.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20,c;
+		c=a+b;
+		System.out.println("+c");
+
+
+	}
+
+}
